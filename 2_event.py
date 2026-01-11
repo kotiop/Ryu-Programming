98e53b94bd335e689b732ea5c5b433c2106c20e6
sudeepSubedi01 <sudip.subedi99@gmail.com>
2026-01-11 13:51:15 +0545

    event handler

---
+++ 2_event.py
@@ -0,0 +1,13 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+
+class MyRyuApp(app_manager.RyuApp):
+    def __init__(self, *_args, **_kwargs):
+        super(MyRyuApp, self).__init__(*_args, **_kwargs)
+        print("Ryu app has started")
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_connected(self, ev):
+        print("Switch Connected")
